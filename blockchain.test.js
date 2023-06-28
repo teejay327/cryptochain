11ce728785b29b8dd81abdb3e79c5a04e899996b
teejay327 <mrenergy@hotmail.com>
2023-06-29 00:22:05 +1000

    Added blockchain creation files

---
+++ blockchain.test.js
@@ -0,0 +1,23 @@
+const Blockchain = require('./blockchain');
+const Block = require('./block');
+
+describe('Blockchain', () => {
+  const blockchain = new Blockchain();
+
+  it('contains a chain Array instance', () => {
+    expect(blockchain.chain instanceof Array).toBe(true);
+  });
+
+  it('starts with the genesis block', () => {
+    expect(blockchain.chain[0]).toEqual(Block.genesis());
+  });
+
+  it('adds a new block to the chain', () => {
+    const newData = 'test1';
+    blockchain.addBlock({ data: newData });
+
+    expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
+  });
+
+
+})
